a8b2bfc5e88c6ef00fd61f26fac91887df9e4729
toma-demagn <44348741+toma-demagn@users.noreply.github.com>
2024-01-24 09:46:08 +0100

    modified venv script

--- back/run_tests.sh
+++ back/run_tests.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-# Create a new virtual environment
+# To run unit tests with venv
+
 python3 -m venv env
-
-# Activate the virtual environment
 source env/bin/activate
-
-# Install requirements
 pip install -r requirements.txt
-
-# Run tests
 pytest tests
 
-# Deactivate the virtual environment
 deactivate
